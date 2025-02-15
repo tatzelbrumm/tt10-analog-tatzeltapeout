9716d8cc5e7286dccc7d42d87e9d7c412bb00e71
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2025-02-15 04:22:15 +0100

    mag/magic_init_project.tcl : set to 2x2 tiles with 3.3V supply rail

--- mag/magic_init_project.tcl
+++ mag/magic_init_project.tcl
@@ -4,12 +4,12 @@
 # Description: This script initializes a new Magic project for an analog design on Tiny Tapeout.
 
 # Important: before running this script, download the the .def file from
-# https://raw.githubusercontent.com/TinyTapeout/tt-support-tools/tt08/def/analog/tt_analog_1x2.def
+# https://raw.githubusercontent.com/TinyTapeout/tt-support-tools/tt08/def/analog/tt_analog_2x2_3v3.def
 
 # Change the settings below to match your design:
 # ------------------------------------------------
 set TOP_LEVEL_CELL     tt_um_analog_example
-set TEMPLATE_FILE      tt_analog_1x2.def
+set TEMPLATE_FILE      tt_analog_2x2_3v3.def
 set POWER_STRIPE_WIDTH 2um                 ;# The minimum width is 1.2um
 
 # Power stripes: NET name, x position. You can add additional power stripes for each net, as needed.
@@ -18,7 +18,7 @@ set POWER_STRIPES {
     VGND  4um
 }
 # If you use the 3v3 template, uncomment the line below:
-#lappend POWER_STRIPES VAPWR 7um
+lappend POWER_STRIPES VAPWR 7um
 
 # Read in the pin positions
 # -------------------------
